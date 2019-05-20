74b7e09ddce8193b1a1adc81dd5a1d036b3458c9
kurayoshi981 <tachp981@yahoo.co.jp>
2019-05-20 11:15:25 +0900

    Add:postのバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,39 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true,
+    length: { minimum: 2, maximum: 100, allow_blank: true}
+
+  validates :profile, presence: true,
+    length: { maximum: 1000, allow_blank: true}
+
+  validates :birthday, presence: true
+
+  validates :age, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than: 18,
+      less_than: 65,
+      allow_blank: true}
+
+  validates :sex, inclusion: {in: [1, 2]}
+
+  validates :job_type, presence: true,
+    length: { maximum: 255, allow_blank: true}
+
+  validates :work_location, presence: true,
+    length: { maximum: 255, allow_blank: true}
+
+  validates :skill, presence: true,
+    length: { maximum: 255, allow_blank: true}
+
+  validates :employment_status, presence: true,
+    length: { maximum: 255, allow_blank: true}
+
+  validates :salary, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than: 1,
+      less_than: 9999,
+      allow_blank: true}
 end
